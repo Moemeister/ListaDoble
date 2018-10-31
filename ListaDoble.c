26dfe2b8a366521588bea69c7a29907124755687
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2018-10-30 21:44:26 -0600

    Starting Insert Funtion

--- ListaDoble.c
+++ ListaDoble.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Nodo{
     int valor;
     Nodo* sig;
     Nodo* ant;
 }Nodo;
-
-
+Nodo* inicio = NULL;
+Nodo* final = NULL;
+//FUNCION PARA GENERAR UN NÚMERO RAMDOM
+int generateRamdomNumber(){
+     return rand()%100;
+}
+//FUNCION CREAR NODO
+Nodo* createNodo(){
+    Nodo* n = (Nodo*)malloc(sizeof(Nodo));
+    return n;
+}
+void insertI(int x){
+    Nodo* n = createNodo();
+    n->valor = x;
+    n->sig = NULL;
+    n->ant = NULL;
+    
+}
 int main(){
-
+    time_t t;
+    srand((unsigned)time(&t));
+    while (1){
+        printf("1-insertar al inicio\n");
+        printf("2-insertar al final\n");
+        printf("3-eliminar en i\n");
+    }
 
     return 0;
 }
